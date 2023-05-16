ede864183038f42ab53a8dae0ccfeee4021902e1
katsuhiro-hayasaka-orico <133623045+katsuhiro-hayasaka-orico@users.noreply.github.com>
2023-05-16 12:37:10 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def communicate():
     )
 
     while not response["choices"]:
-        response = openai.ChatCompletion.fetch(model="gpt-3.5-turbo", response["id"])
+        response = openai.ChatCompletion.fetch(response["id"], model="gpt-3.5-turbo")
 
     bot_message = response["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": bot_message})
